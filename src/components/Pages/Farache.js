5b9f4dce7377cb2bc57bb74d635fe96e7086c426
Hossein Narimani Rad <hossein.narimani.rad@gmail.com>
2019-04-23 18:14:18 +0430

    1398.02.03

---
+++ src/components/Pages/Farache.js
@@ -0,0 +1,27 @@
+﻿import React, { Component } from 'react';
+import { render } from "react-dom";
+
+//import FileUpload from '../Common/FileUpload'
+import Upload from '../upload/Upload'
+import './main.css'
+
+
+class MainComponent extends React.Component {
+
+    render() {
+        return (
+            <div className="height100Percent rtl"> 
+                <div className="shadowPanel curvePanel margin4 horizontallyCenter height100Percent">
+                    <br />
+                    <h2 className="text-right">آماده‌سازی داده</h2>
+                    <div className="dragDropPanel">
+                        {/* <FileUpload /> */}
+                        <Upload/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MainComponent;
